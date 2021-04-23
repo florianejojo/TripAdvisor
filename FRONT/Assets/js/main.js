1d3c441bf3edcfd10211811eb4cc7882ac596e07
Floriane Lefebvre <flo.le@icloud.com>
2021-04-23 23:40:10 +0200

    bouton contacter-nous fonctionnel

--- FRONT/Assets/js/main.js
+++ FRONT/Assets/js/main.js
@@ -1,28 +1,43 @@
 const $ = document;
 
-// let button = $.querySelector("#formButton");
+// let button = $.querySelector("#headerButton");
 // var modal = $.querySelector(".modal");
 // var backdrop = $.querySelector(".backdrop");
 
 $.addEventListener("DOMContentLoaded", () => {
     console.log("page chargée ");
 
-    $.querySelector("#formButton").addEventListener("click", async () => {
+    $.querySelector("#headerButton").addEventListener("click", () => {
         console.log("click");
-        $.querySelector(".behindModal").style.display = "none";
 
+        $.querySelector(".behindModal").style.display = "none";
         $.querySelector(".backdrop").style.display = "block";
         $.querySelector(".modal").style.display = "flex";
 
-        // const data = {
-        //     firstName: $.querySelector("#firstName").value,
-        //     lastName: $.querySelector("#lastName").value,
-        //     email: $.querySelector("#email").value,
-        //     message: $.querySelector("#message").value,
-        // };
+        $.querySelector("#formButton").addEventListener("click", async () => {
+            const data = {
+                firstName: $.querySelector("#firstName").value,
+                lastName: $.querySelector("#lastName").value,
+                email: $.querySelector("#email").value,
+                message: $.querySelector("#message").value,
+            };
+            console.log(data);
 
-        // const response = await axios.post("http://localhost:3000/form", data);
-        // console.log(response);
-        // alert("Merci");
+            $.querySelector(".behindModal").style.display = "inherit";
+            $.querySelector(".backdrop").style.display = "none";
+            $.querySelector(".modal").style.display = "none";
+
+            // const response = await axios.post(
+            //     "http://localhost:3000/form",
+            //     data
+            // );
+            // console.log(response);
+            // alert("Merci");
+        });
+        $.querySelector(".backdrop").addEventListener("click", async () => {
+            $.querySelector(".behindModal").style.display = "inherit";
+            $.querySelector(".backdrop").style.display = "none";
+            $.querySelector(".modal").style.display = "none";
+        });
     });
 });
